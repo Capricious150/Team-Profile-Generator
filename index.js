08bf6f9b265053985ecdd718151d6573c9fe6e77
Austin A <austinandrews89@gmail.com>
2022-03-07 17:43:56 -0700

    Continued dummy programming. Arrow function in generateHTML successful!

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const jest = require('jest');
+const generateHTML = require('./src/generateHTML')
+
+function testWrite(){
+    fs.writeFile("sample_HTML.html", generateHTML.generateHTML(), (err) => {
+        (err) ? console.log(err) : console.log("Callback executed, file created!")
+    })
+}
+
+testWrite();
